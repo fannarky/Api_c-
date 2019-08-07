9b7ec095c2b7e77c6158dcb2e53c2642e5b01a17
Phelipe Soares Simeao <pssimeao@stefanini.com>
2019-08-07 15:31:08 -0300

    começando o controller do computador

---
+++ Teste_BackEnd/Teste_BackEnd/Controllers/ComputerController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using Teste_BackEnd.Model;
+
+namespace Teste_BackEnd.Controllers
+{
+
+    [Route("api/[controller]")]
+    public class ComputerController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<Computer>> Get()
+        {
+            return new List<Computer>();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Computer> GetByPk(int id)
+        {
+            return new Computer();
+        }
+
+        [HttpPost]
+        public ActionResult<MessageCollection> Post([FromBody] Computer computer)
+        {
+            return new MessageCollection();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult<MessageCollection> DeleteByPk(int id)
+        {
+            return new MessageCollection();
+        }
+
+        [HttpPut]
+        public ActionResult<MessageCollection> Put([FromBody] Computer computer)
+        {
+            return new MessageCollection();
+        }
+
+
+
+    }
+}
